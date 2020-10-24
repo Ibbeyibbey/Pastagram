ba3ccfe1ce0f9d323d9f1cbca12291aca156615e
Isha B <Girllovestech93@gmail.com>
2020-10-23 21:43:32 -0700

    "authentication portion of app"

--- Pastagram/AppDelegate.swift
+++ Pastagram/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +16,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // --- Copy this only
+            
+            let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "go8e6G805KU95q5GMAOOqHebXCouBx4lr2rmXl5D" // <- UPDATE
+                    $0.clientKey = "oPY73hLNgxcLNxzQpCKVv1Y7uog7tX6Ojc9H5LU2" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+            
+            // --- end copy
+        
+        
+        
+        
         return true
+        
+        
+        
+        
+        
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
